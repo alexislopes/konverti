da9975e024417d164df322fce8744db5ec69fdda
Alexis Lopes <alexisdrops@gmail.com>
2023-09-30 01:31:40 -0300

    Updates

---
+++ _nuxt/config.8dc7a8e8.js
@@ -0,0 +1,1 @@
+import{U as p,V as o,W as e,u as f}from"./entry.ddc87128.js";const i=p({myLayer:{name:"Hello from Nuxt layer"}}),a={},t=o(i,a);function u(){const n=f();return n._appConfig||(n._appConfig=e(t)),n._appConfig}export{u};
