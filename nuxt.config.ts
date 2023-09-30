7fdeb6591e28a532d4351f6fda60ff25944810b2
Alexis Lopes <alexisdrops@gmail.com>
2023-09-30 01:32:25 -0300

    prepare for deploy 🚀

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/nubank-to-mobills-converter/' // baseURL: '/<repository>/'
+  },
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', 'nuxt-icon'],
   pages: true
 });
