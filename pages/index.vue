edff612547e71fe5e0ab664001b6510a19866608
Alexis Lopes <alexisdrops@gmail.com>
2023-10-04 00:14:41 -0300

    added datepicker to filter for expenses 🗓️

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup>
+
+useHead({
+  title: "Konverti by Alexis Lopes",
+  link: [{ rel: 'icon', type: 'image/png', href: '/favicon.ico' }]
+})
+
 import download from "downloadjs";
 import Papa from "papaparse";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 const csv = ref([]);
 const account = ref('');
 const ocultados = ref([])
@@ -15,6 +21,47 @@ const contas = useLocalStorage('contas', [], {
   },
 });
 
+const parseDate = (date = "") => {
+
+  date = new Date(
+    ...date
+      .split("/")
+      .map((m) => Number(m))
+      .reverse()
+  );
+  return new Date(date.setMonth(date.getMonth() - 1));
+}
+
+const date = ref({
+  start: new Date(),
+  end: new Date(),
+})
+
+const attrs = ref([
+  {
+    key: 'today',
+    highlight: {
+      color: '#7c3aed',
+      fillMode: 'solid'
+    },
+    dates: new Date()
+  }
+])
+
+const rules = ref({
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  milliseconds: 0,
+});
+
+const filtered = computed(() => {
+  return csv.value.filter(m => {
+    const data = new Date(parseDate(m.Data))
+    return data.getTime() >= date.value.start.getTime() && data.getTime() <= date.value.end.getTime()
+  })
+})
+
 const lastImport = useLocalStorage('lastImport', 0)
 
 const handleFile = (file: File) => {
@@ -23,24 +70,19 @@ const handleFile = (file: File) => {
     skipEmptyLines: true,
     complete: function (results: any) {
       csv.value = results.data.map(item => { return { ...item, Conta: account.value, Categoria: "" } });
+      const datas = [... new Set(csv.value.map(m => parseDate(m.Data).getTime()))]
+      date.value = {
+        start: new Date(Math.min(...datas)),
+        end: new Date(Math.max(...datas)),
+      }
     }.bind(this)
   });
 }
 
-const headers = computed(() => {
-  if (csv.value.length > 0) {
-    return Object.keys(csv.value[0]);
-  }
-  return [];
-});
 
 const downloadExemplo = () => {
   const header = ['Data', 'Descrição', 'Valor', 'Conta', 'Categoria']
-  const rows = csv?.value.map(m => { return { ...m, Conta: account.value } }).map(dado => header.map(k => `"${dado[k]}"`))
-  console.log(rows)
-
-  // const header = keys.map(m => props.colunas.find(f => f.key === m)?.nome.normalize("NFD").replace(/[^a-zA-Z\s]/g, ""))
-  // console.log(header)
+  const rows = filtered?.value.map(m => { return { ...m, Conta: account.value } }).map(dado => header.map(k => `"${dado[k]}"`))
 
   const convertedcsv = [
     header.map(m => `"${m}"`), // header row first
@@ -49,7 +91,6 @@ const downloadExemplo = () => {
     .map((m) => m.join(";"))
     .join("\r\n");
 
-  console.log(convertedcsv)
   download(convertedcsv, `NUB2MOB Converter for ${account.value} at ${new Date().toLocaleDateString(navigator.language, { day: 'numeric', month: '2-digit', year: '2-digit', hour: 'numeric', minute: 'numeric' })}.csv`, "text/csv");
 
   lastImport.value = new Date().getTime();
@@ -59,10 +100,26 @@ const downloadExemplo = () => {
 watch(files, (files) => {
   handleFile(files[0])
 })
+
+
+
 </script>
 
 <template>
-<div class="flex items-center justify-center h-screen">
+<div class="flex items-center justify-center h-screen flex-col gap-6">
+  <VDatePicker isDark v-model.range="date" :attributes="attrs" :max-date="new Date()" color="indigo" :rules="rules" :disabled="!filtered.length">
+    <template #default="{ togglePopover, inputValue, inputEvents }">
+      <div class="flex rounded-lg border border-gray-300 dark:border-gray-600 overflow-hidden cursor-pointer"
+        @click="togglePopover">
+        <button
+          class="flex justify-center items-center px-2 bg-accent-100 hover:bg-accent-200 text-accent-700 border-r border-gray-300 dark:bg-gray-700 dark:text-accent-300 dark:border-gray-600 dark:hover:bg-gray-600">
+          <Icon name="octicon:calendar-16" class="text-xl" />
+        </button>
+        <input readonly :value="`${inputValue.start} - ${inputValue.end}`" v-on="inputEvents"
+          class="flex-grow px-2 py-1 bg-white dark:bg-gray-700 cursor-pointer" />
+      </div>
+    </template>
+  </VDatePicker>
   <dialog id="my_modal_1" class="modal">
     <div class="modal-box flex justify-between items-center">
       <NovaConta />
